8a9547804e3cc28f108838bb026ededb49266454
fmoyen <fabrice_moyen@fr.ibm.com>
2017-09-06 11:44:22 +0200

    reset_juju_demo.bash: add -n option (not starting after reset for backup purpose)

--- JUJU/demo_scripts/reset_juju_demo.bash
+++ JUJU/demo_scripts/reset_juju_demo.bash
@@ -13,10 +13,32 @@
 #
 ###########################################################################
 
-echo
-echo
 
 SnapshotName="DEMO16.04.2"
+
+option_n=0
+while getopts "nh" option
+do
+  case $option in
+    n  )
+      option_n=1
+    ;;
+    h  )
+      echo
+      echo "USAGE:"
+      echo "$0 [-n]"
+      echo "    -n (optional): if you don't want to start jdmaas/jdjuju VMs after reset (for backup purpose)"
+      echo
+      exit 1
+    ;;
+    \? ) echo "Unknown option: -$OPTARG" >&2; exit 1;;
+    :  ) echo "Missing option argument for -$OPTARG" >&2; exit 1;;
+    *  ) echo "Unimplemented option: -$OPTARG" >&2; exit 1;;
+  esac
+done
+
+echo
+echo
 
 echo "Stopping jdmaas/jdjuju Virtual Machines..."
 for i in jdmaas jdjuju
@@ -44,13 +66,21 @@ echo "Reverting the jdmaas/jdjuju Virtual Machines to the snapshot $SnapshotName
 virsh snapshot-revert jdmaas --snapshotname $SnapshotName
 virsh snapshot-revert jdjuju --snapshotname $SnapshotName
 
-echo "Starting jdmaas/jdjuju Virtual Machines..."
-for i in jdmaas jdjuju
-do
-  virsh start $i
-done
+if [ $option_n == 0 ]
+then
+  echo "Starting jdmaas/jdjuju Virtual Machines..."
+  for i in jdmaas jdjuju
+  do
+    virsh start $i
+  done
 
+  echo
+  echo "Wait for 1 or 2 min and everything should be OK and ready."
+  sleep  60
+else
+  echo "Warning: jdmaas/jdjuju Virtual Machines NOT STARTED..."
+  echo "The demo is not available (but ready for backup)"
+fi
 echo
-echo "Wait for 1 or 2 min and everything should be OK and ready."
 echo "Bye"
 echo
