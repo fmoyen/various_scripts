02f2057922fa5eb8f35678e4d5e9ec006958bcce
fmoyen <fabrice_moyen@fr.ibm.com>
2018-08-22 11:12:47 +0200

    chage script

---
+++ Age/passwords_age.bash
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+Accounts="fabrice root rpc"
+Today=`date +%Y-%m-%d`
+
+#####################################################################################
+# Check if root
+#
+
+if [ `whoami` != "root" ]
+then
+  echo
+  echo "You're not root. You need to execute this script as root"
+  echo "Think of sudo command..."
+  echo "Exiting"
+  exit 0
+fi
+
+#####################################################################################
+# MAIN
+
+echo
+for i in $Accounts
+do
+   echo "$i account"
+   echo "----------------------"
+   chage -d $Today $i
+   chage -W 15 $i
+   chage -M 90 $i
+   chage --list $i
+   echo
+done
