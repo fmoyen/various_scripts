2876fd0484a3b375b8eff3ddea46eb3b0ba46265
Fabrcie MOYEN <fmoyen@lewen05.dcry.iccmop>
2020-02-19 14:57:43 +0100

    CAPI_adduser
    Signed-off-by: Fabrcie MOYEN <fmoyen@lewen05.dcry.iccmop>

--- CAPI_adduser/CAPI_adduser.bash
+++ CAPI_adduser/CAPI_adduser.bash
@@ -2,23 +2,64 @@
 
 UsersFile=./CAPI_users.txt
 
+# STEPS:
+#  - check if root
+#  - check /home/capiteam mounted
+#  - check if /home/capiteam/$Name exists if remote
+#  - check if /home/$Name exists if local
+#  - check if $Name, $ID, $GID exist
+#  - create user or group
+#  - set password if user
+#  - configure sudo with or w/o password
+
+
 n=0
 while read Line
 do
-  User=($Line)
-  if [ ${User[0]} != "#" ]; then
-    echo "User $n"
-    echo "-------"
-    Name=${User[0]}; echo "NAME= $Name"
-    ID=${User[1]}; echo "ID= $ID"
-    GID=${User[2]}; echo "GID= $GID"
-    Local=${User[4]}; echo "Capiteam or local= $Local"
 
-    grep $Name /etc/passwd
-    grep $ID /etc/passwd
-    grep $GID /etc/group
+  User=($Line)
+
+  if [ ${User[0]} = "#" ]; then 
+    n=$((n+1))
+    continue
   fi
 
   echo
+  echo "====================================================================================="
+  echo "User $n"
+  echo "-------"
+  Name=${User[0]}; echo "NAME= $Name"
+  ID=${User[1]}; echo "ID= $ID"
+  GID=${User[2]}; echo "GID= $GID"
+  Local=${User[4]}; echo "Capiteam or local= $Local"
+
+  if [ $ID != "XXXX" ]; then
+    echo;echo "/etc/passwd:"
+    echo "------------"
+    grep $Name /etc/passwd; NameRC=$?
+    grep $ID /etc/passwd; IDRC=$?
+  fi
+  echo;echo "/etc/group:"
+  echo "-----------"
+  grep $GID /etc/group; GIDRC=$?
+
+  echo
+
+  if [ $ID != "XXXX" ]; then
+    if [ $NameRC -ne 0 ] && [ $IDRC -ne 0 ] && [ $GIDRC -ne 0 ]; then
+      echo "  --> OK to create User $Name"
+    else
+      echo "  --> Cannot Create User $Name. Please check"
+    fi
+  else
+    if [ $GIDRC -ne 0 ]; then
+      echo "  --> OK to create Group $Name"
+    else
+      echo "  --> Cannot Create Group $Name. Please check"
+    fi
+  fi
+
   n=$((n+1))
 done < $UsersFile
+
+echo
