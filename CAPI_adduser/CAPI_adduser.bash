38778a11592d7e99ab7f69fffcd2f66e4c0f4eca
fmoyen <fabrice_moyen@fr.ibm.com>
2020-09-16 17:13:15 +0200

    CAPI_adduser/CAPI_adduser.bash: useradd instead of adduser + chpasswd
    Signed-off-by: fmoyen <fabrice_moyen@fr.ibm.com>

--- CAPI_adduser/CAPI_adduser.bash
+++ CAPI_adduser/CAPI_adduser.bash
@@ -32,7 +32,8 @@ do
   # Variables
   Name=${User[0]}; echo -e "NAME= $Name \t\c"
   ID=${User[1]}; echo -e "ID= $ID \t\c"
-  GID=${User[2]}; echo "GID= $GID"
+  GID=${User[2]}; echo -e "GID= $GID \t\c"
+  Password=${User[3]}; echo "Password= $Password"
   Local=${User[4]}; echo "Capiteam or local= $Local"
   MoreGroup=${User[5]}; echo "Additional group= $MoreGroup"
 
@@ -74,7 +75,7 @@ do
       echo "Command: $CMD" 
       eval $CMD
       echo "--> OK to create User $Name with Group $Name"
-      CMD="adduser $AdduserOptions --home $Homedir --uid=$ID --gid=$GID $Name"
+      CMD="useradd $AdduserOptions --home-dir $Homedir --uid=$ID --gid=$GID $Name"
       echo "Command: $CMD" 
       eval $CMD
       if [ $MoreGroup != "XXXX" ]; then
@@ -83,6 +84,10 @@ do
         echo "Command: $CMD" 
         eval $CMD
       fi
+      echo "--> OK to change User $Name password with  password: $Password"
+      CMD="echo \"$Name:$Password\" | chpasswd"
+      echo "Command: $CMD" 
+      eval $CMD
 
     else
       echo "--> Cannot Create User $Name. Please check"
