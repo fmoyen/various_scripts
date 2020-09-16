7da8d7362fcd89d94f3995f3590035935169fbbf
fmoyen <fabrice_moyen@fr.ibm.com>
2020-09-16 16:17:08 +0200

    CAPI_adduser
    Signed-off-by: fmoyen <fabrice_moyen@fr.ibm.com>

--- CAPI_adduser/CAPI_adduser.bash
+++ CAPI_adduser/CAPI_adduser.bash
@@ -2,7 +2,7 @@
 
 UsersFile=./CAPI_users.txt
 
-# STEPS:
+# STEPS WE SHOULD DO:
 #  - check if root
 #  - check /home/capiteam mounted
 #  - check if /home/capiteam/$Name exists if remote
@@ -32,7 +32,20 @@ do
   ID=${User[1]}; echo "ID= $ID"
   GID=${User[2]}; echo "GID= $GID"
   Local=${User[4]}; echo "Capiteam or local= $Local"
+  MoreGroup=${User[5]}; echo "Additional group= $MoreGroup"
+  AdduserOptions="--group"
+  AddgroupOptions=""
+  if [[ $Local == "capiteam" ]]; then
+    Homedir=/home/$Local/$Name
+    AdduserOptions="$AdduserOptions --no-create-home"
+  elif [[ $Local == "local" ]]; then
+    Homedir=/home/$Name
+  else
+    echo "$Local case not known by script. Aborting..."
+    exit 1
+  fi
 
+  # Test if we can create User/Group
   if [ $ID != "XXXX" ]; then
     echo;echo "/etc/passwd:"
     echo "------------"
@@ -42,22 +55,34 @@ do
   echo;echo "/etc/group:"
   echo "-----------"
   grep $GID /etc/group; GIDRC=$?
+  grep $Name /etc/group; GNameRC=$?
 
   echo
 
+  # We create User/Group
   if [ $ID != "XXXX" ]; then
-    if [ $NameRC -ne 0 ] && [ $IDRC -ne 0 ] && [ $GIDRC -ne 0 ]; then
-      echo "  --> OK to create User $Name"
+    if [ $NameRC -ne 0 ] && [ $IDRC -ne 0 ] && [ $GIDRC -ne 0 ] && [ $GNameRC -ne 0 ]; then
+      echo "  --> OK to create User $Name with Group $Name"
+      CMD="adduser $AdduserOptions --home $Homedir --uid=$ID --gid=$GID $Name"
+      echo "Command: $CMD" 
+      if [ $MoreGroup != "XXXX" ]; then
+        echo "  --> OK to add group $MoreGroup to the User $Name"
+        CMD="usermod -aG $MoreGroup $Name"
+        echo "Command: $CMD" 
+      fi
     else
       echo "  --> Cannot Create User $Name. Please check"
     fi
   else
     if [ $GIDRC -ne 0 ]; then
       echo "  --> OK to create Group $Name"
+      CMD="groupadd $AddgroupOptions --gid=$GID $Name"
+      echo "Command: $CMD" 
     else
       echo "  --> Cannot Create Group $Name. Please check"
     fi
   fi
+
 
   n=$((n+1))
 done < $UsersFile
